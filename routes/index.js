1804c8ee462c74351797905be3fc61464ab44b81
Luvkitri <janjedrzejewski@gmail.com>
2021-04-27 22:30:42 +0200

    Added route for handling sending messages to sqs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const AWS = require('aws-sdk');
 
-const { fetchFilesFromS3, fetchFileFromS3 } = require('../utils/aws');
+const {
+  fetchFilesFromS3,
+  fetchFileFromS3,
+  createMessage,
+  sendMessagesToQueue,
+} = require('../utils/aws');
 
 router.get('/', async (req, res) => {
   const data = await fetchFilesFromS3();
@@ -13,7 +18,15 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/files', async (req, res) => {
-  console.log(req.body);
+  try {
+    let files = Object.values(req.body);
+    const messages = files.map(createMessage)
+    await sendMessagesToQueue(messages)
+    res.status(200);
+  } catch (error) {
+    console.log(error);
+  }
+  
 });
 
 router.post('/content', async (req, res) => {
